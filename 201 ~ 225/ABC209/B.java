9c988034a99ad5a906786ebd9a07ed19b0dc0342
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-07-10 22:56:17 +0900

    java

---
+++ 201 ~ 225/ABC209/B.java
@@ -0,0 +1,30 @@
+package ABC209;
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int[] num = new int[a];
+        for (int i = 0; i < num.length; i++) {
+            num[i] = scan.nextInt();
+            // System.out.println(num[i]);
+        }
+        scan.close();
+        // System.out.println(num);
+        for (int i = 0; i < a; i++) {
+            if (i % 2 == 1) {
+                num[i]--;
+            }
+            b -= num[i];
+        }
+        if (b < 0) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
+    }
+}
--- 201 ~ 225/Main.java
+++ 201 ~ 225/Main.java
@@ -5,42 +5,41 @@ import java.util.InputMismatchException;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		//* AtCoder提出用
+		// * AtCoder提出用
 		// try{
-			Scanner scan = new Scanner(System.in);
-			int a = scan.nextInt();
-			int b = scan.nextInt();
-			scan.close();
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		scan.close();
 
-			int ans = a + b;
-			ans = Math.max(ans, a - b);
-			ans = Math.max(ans, a * b);
-			System.out.println(ans);
+		int ans = a + b;
+		ans = Math.max(ans, a - b);
+		ans = Math.max(ans, a * b);
+		System.out.println(ans);
 
 		// }catch(InputMismatchException e){
-		// 	//* ファイル読み込み用
-		// 	try {
+		// //* ファイル読み込み用
+		// try {
 
-		// 		File file = new File("../txt/A.txt");
-		// 		Scanner scanner = new Scanner(file);
-		// 		// while (scanner.hasNext()){
-		// 			// 	String dat = scanner.next();
-		// 			// 	System.out.println(dat);
-		// 			// }
-		// 			int a = scanner.nextInt();
-		// 			int b = scanner.nextInt();
-		// 			scanner.close();
-					
-		// 			int ans = a + b;
-		// 			ans = Math.max(ans, a - b);
-		// 			ans = Math.max(ans, a * b);
-		// 			System.out.println(ans);
+		// File file = new File("../txt/A.txt");
+		// Scanner scanner = new Scanner(file);
+		// // while (scanner.hasNext()){
+		// // String dat = scanner.next();
+		// // System.out.println(dat);
+		// // }
+		// int a = scanner.nextInt();
+		// int b = scanner.nextInt();
+		// scanner.close();
 
-		// 		} catch(FileNotFoundException f) {
-		// 			System.out.println(f);
-		// 			System.out.println("----------------");
-		// 			System.out.println(e);
-		// 		}
-		}
+		// int ans = a + b;
+		// ans = Math.max(ans, a - b);
+		// ans = Math.max(ans, a * b);
+		// System.out.println(ans);
+
+		// } catch(FileNotFoundException f) {
+		// System.out.println(f);
+		// System.out.println("----------------");
+		// System.out.println(e);
+		// }
 	}
 }
