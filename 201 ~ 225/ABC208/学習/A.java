b89c93ff6e8fb2b719adfb20f75210dd4e908e33
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-07-15 22:15:48 +0900

    s

--- 201 ~ 225/ABC208/学習/A.java
+++ 201 ~ 225/ABC208/学習/A.java
@@ -6,14 +6,14 @@ public class A {
     public static void main(String[] args) throws Exception {
         File file = new File("../txt/A.txt");
         Scanner scan = new Scanner(file);
-        //* 学習用scan
-        //* ------------------------------------------------
+        // * 学習用scan
+        // * ------------------------------------------------
         int a = scan.nextInt();
         int b = scan.nextInt();
         scan.close();
 
-        //* NG例：a:3, b:2のケース
-        if(a <= b && b <= a * 6) {
+        // * NG例：a:3, b:2のケース
+        if (a <= b && b <= a * 6) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
