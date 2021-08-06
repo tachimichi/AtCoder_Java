51e028c20d58c21024391f94491d0f8be0281694
tachi_michi@revolve <defxyz714@gmail.com>
2021-08-06 14:35:56 +0900

    201B

--- 201 ~ 225/ABC201/学習/B.java
+++ 201 ~ 225/ABC201/学習/B.java
@@ -8,21 +8,26 @@ public class B {
         // * 学習用scan
         // * ---------------------------
         int n = scan.nextInt();
-        //* String型として、標準入力
-        //* 配列もString型で格納する
-        String strNum = scan.next();
-        String a[] = strNum.split("");
-        
-        for(int i =0; i < n; i++) {
-            if(a[i].equals("1")) {
-                if(i % 2 == 0) {
-                    System.out.println("Takahashi");
-                } else {
-                    System.out.println("Aoki");
-                }
-                //* もし1を発見できた場合
-                break;
-            }
+        // 高さを格納する配列
+        int[] a = new int[n];
+        // map(キー、値)
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String str = scan.next();
+            int high = scan.nextInt();
+            a[i] = high;
+            map.put(high, str);
         }
+        scan.close();
+
+        // System.out.println(map);
+        Arrays.sort(a);
+        // 配列の中身を確認
+        // System.out.println(Arrays.toString(a));
+        // 配列の要素を指定
+        // System.out.println(a[a.length-2]);
+
+        System.out.println(map.get(a[a.length-2]));
+
     }
 }
